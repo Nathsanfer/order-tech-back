7b82d612a1b581fa3aa8bd2e8f4df6948fbdc7a4
NathSanFer <158168917+Nathsanfer@users.noreply.github.com>
2025-11-04 10:44:03 -0300

    feat: create project

---
+++ prisma/migrations/20251104134047_init/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the `pontos_turisticos` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "pontos_turisticos";
+PRAGMA foreign_keys=on;
